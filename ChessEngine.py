8e8aed25ece9a64f96de1afbbbb3dbdc9e1a1c4c
abay.ongar <abay.ongar.botabek@gmail.com>
2024-02-20 12:56:28 +0600

    king move lesson7

--- ChessEngine.py
+++ ChessEngine.py
@@ -238,15 +238,29 @@ class GameState():
     '''Get all the King moves for the pawn located at row, col and add these moves to the list'''
 
     def getKingMoves(self, r, c, moves):
-        kingMoves = ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1))
+        rowMoves = (-1, -1, -1, 0, 0, 1, 1, 1)
+        colMoves = (-1, 0, 1, -1, 1, -1, 0, 1)
         allyColor = "w" if self.whiteToMove else "b"
         for i in range(8):
-            endRow = r + kingMoves[i][0]
-            endCol = c + kingMoves[i][1]
+            endRow = r + rowMoves[i]
+            endCol = c + colMoves[i]
             if 0 <= endRow < 8 and 0 <= endCol < 8:
                 endPiece = self.board[endRow][endCol]
-                if endPiece[0] != allyColor: #not an ally piece (empty or enemy piece)
-                    moves.append(Move((r, c), (endRow, endCol), self.board))
+                if endPiece[0] != allyColor: #not an ally piece 
+                    #place king on end squre and check for checks 
+                    if allyColor == 'w':
+                        self.whiteKingLocation = (endRow, endCol)
+                    else:
+                        self.blackKingLocation = (endRow, endCol)
+                    inCheck, pins, checks = self.checkForPinsAndChecks()
+                    if not inCheck:
+                        moves.append(Move((r, c), (endRow, endCol), self.board))
+                    #place king back on original location
+                    if allyColor == 'w':
+                        self.whiteKingLocation = (r, c)
+                    else:
+                        self.blackKingLocation = (r, c)
+    
 
     '''Get all the Bishop moves for the pawn located at row, col and add these moves to the list'''
 
